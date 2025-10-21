5036a894dc0f28a9d642f293c7360290b62e22b4
andrii-piddub <andrii.piddubchyshyn@gmail.com>
2025-10-21 19:29:56 +0300

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -5,13 +5,18 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title':'Home',
-        'content':'Main page - Home',
-        'list':['first','second'],
-        'dict':{'first':1},
-        'bool':True
+        'title':'Home - Main ',
+        'content':'Furniture shop - Home',
+   
     }
     return render(request, 'main/index.html',context)
 
 def about(request):
-    return HttpResponse('About us')
+    context = {
+        'title':'Home - About ',
+        'content':'About our shop',
+        'text_on_page':'Our furniture shop offers high-quality, stylish, and durable pieces designed to bring comfort and elegance to any space. We focus on combining modern design with craftsmanship to help customers create homes they truly love.'
+   
+    }
+
+    return render(request, 'main/about.html',context)
