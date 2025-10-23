b35cebae66174d48dbbb80d783a5eb47ca7c3a9c
andrii-piddub <andrii.piddubchyshyn@gmail.com>
2025-10-23 09:58:18 +0300

    slug commit for categories

--- goods/views.py
+++ goods/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
 
 # Create your views here.
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
     context = {
         'title':'Home - Catalog',
         'goods': goods
@@ -12,5 +15,9 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request,product_slug):
+    product = Products.objects.get(slug=product_slug)
+    content = {
+        'product':product
+    }
+    return render(request, 'goods/product.html',content)
