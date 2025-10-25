c3a53c7435d4c6a010820d0cc7d32c62c9565036
andrii-piddub <andrii.piddubchyshyn@gmail.com>
2025-10-25 13:10:22 +0300

    simple search created

--- goods/views.py
+++ goods/views.py
@@ -2,13 +2,17 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
+from goods.utils import q_search
 
 # Create your views here.
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
+    page = request.GET.get('page',1)
     on_sale = request.GET.get('on_sale', None)
     order_by=request.GET.get('order_by', None)
-    page = request.GET.get('page',1)
-    if category_slug == 'all':
+    query = request.GET.get('q',None)
+    if  query:
+        goods = q_search(query)
+    elif category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
