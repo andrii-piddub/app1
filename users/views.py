e2cee9c0161b72d4ef9e80669b0e0292ff2fccd7
andrii-piddub <andrii.piddubchyshyn@gmail.com>
2025-10-29 09:00:40 +0200

    registration form and controller ready

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
+from xml.dom import UserDataHandler
 from django.contrib import auth
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def login(request):
@@ -19,10 +20,6 @@ def login(request):
             return HttpResponseRedirect(reverse('main:index'))
     else:
             form = UserLoginForm()
-
-    
-    
-
     context = {
         'title':'Home - Login',
         'form':form
@@ -30,8 +27,18 @@ def login(request):
     return render(request,'users/login.html', context)
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request,user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
     context ={
-        'title':'Home - Registration'
+        'title':'Home - Registration',
+        'form':form
     }
     return render(request, 'users/registration.html', context)
 
@@ -43,4 +50,5 @@ def profile(request):
     return render(request,'users/profile.html', context)
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
